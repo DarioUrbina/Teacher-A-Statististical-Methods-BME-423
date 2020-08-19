93857a48f9a40e05595cba5fc6f745ccff214d56
Darío Urbina <urbiname@usc.edu>
2020-08-19 16:17:43 -0700

    Modifications and analysis of class material

--- Week_1_Discussion/Discussion_session_week_1.R
+++ Week_1_Discussion/Discussion_session_week_1.R
@@ -8,8 +8,23 @@ cat("\014") # it means ctrl+L. clear window
 graphics.off() # close all plots
 
 DIR<-"C:/Users/dario/Documents/Github/Teacher-A-Statististical-Methods-BME-423/Week_1_Discussion";
-
 setwd(DIR); # change working directory
+
+X<-c(2,2,2,2); # Vector [2,2,2,2]
+Y<-matrix(,3,2); # 3(row) by 2(column) matrix -no values (NA)
+Y_trans<-t(Y) # transpose
+ncol(Y)# number of columns in a matrix
+nrow(Y) # the number of rows
+M<-matrix(1:6,3,2); # assign 1~6 into the elements of the matrix
+M[2,2]# Find the chosen indexed value in the matrix
+#M[2,2]<-44 #Assigning an specific value
+M[1:2,]# Find several elements from the chosen indices (first and second element from both: columns and rows)
+M[1:2,1]# Find several elements from the chosen indices (first and second elements from rows)
+M[1:2,2]# Find several elements from the chosen indices (first and second elements from rows)
+
+
+
+
 
 ## 2. Load library
 # install.packages("psych")
