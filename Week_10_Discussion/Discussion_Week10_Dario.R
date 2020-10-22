aff27dde239fed65b3512025fb20a24dc9c8729b
Darío Urbina <urbiname@usc.edu>
2020-10-22 09:45:09 -0700

    Changes for Darío's version

--- Week_10_Discussion/Discussion_Week10_Dario.R
+++ Week_10_Discussion/Discussion_Week10_Dario.R
@@ -3,7 +3,6 @@
 #<Regression & correlation>
 
 ## 1. Mandatory lines
-# Similiar thing as clc;clear all;close all in MATLAB
 rm(list = ls()); # clear workspace variables
 cat("\014") # it means ctrl+L. clear window
 graphics.off() # close all plots
@@ -12,16 +11,19 @@ graphics.off() # close all plots
 
 #Load the martians data set
 #Plot a scatter plot of the data
-#Find the linear regression line properties (slope and intercept,  residual error, Standard error of slope and intercept)
+ 
 #Plot the line on the data
-#Find the correlation of the data
-#Provide a 95% CI of the slope and intercept
+#5.- Provide a 95% CI of the slope and intercept
 
-mypath <- "~/Desktop/week10/martians.csv"
+#1.- Find the linear regression line properties (slope and intercept, 
+# 2.- residual error, 
+# 3.- Standard error of slope and intercept)
+# 4.- Find the correlation of the data
+
+mypath <- "~/Github/Teacher-A-Statististical-Methods-BME-423/Week_10_Discussion/martians.csv"
 mydata <- read.csv(mypath,header=TRUE); 
 View(mydata)
-cat("\014")
-
+#cat("\014")
 regression.1 <- lm(formula = Weight ~ Height, data = mydata )
 summary(regression.1)
 coefficients(regression.1)
@@ -32,7 +34,7 @@ plot(mydata$Height, mydata$Weight,ylab="Weight",xlab="Height")
 #Plotting the regression line
 abline(regression.1)
 cor( mydata$Height, mydata$Weight )
-K<-data.frame(Height=c(35, 40, 45))
+K<-data.frame(Height=c(35, 40, 45))         #DUM check the function of this##############
 K$Weight<-predict(regression.1, newdata=K) # Where K = X coordinate, OPTION = confidence / prediction confidence: Line of Means, prediction: observation
 points(K$Height, K$Weight,ylab="Weight",xlab="Height", col="red")
 # Exercise Example: In patients diagnosed with acute dilated
